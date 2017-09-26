da0a09b92d35c332674781b6360b663cbce53ea0
Dean Webb <deanofthewebb@gmail.com>
2017-09-25 20:56:12 -0700

    More tweaks

--- docker_files/run.sh
+++ docker_files/run.sh
@@ -4,10 +4,10 @@ set -e
 
 if [ -z "$1" ]
   then
-    jupyter notebook --no-browser --port=8888
+    jupyter notebook --no-browser --port=8888 --allow-root
 elif [ "$1" == *".ipynb"* ]
   then
-    jupyter notebook --no-browser --port=8888 "$1"
+    jupyter notebook --no-browser --port=8888 --allow-root "$1"
 else
     exec "$@"
 fi
